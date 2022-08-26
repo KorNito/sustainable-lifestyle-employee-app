218978ebb34d277779706ff1e11445723ed787e5
KorNito <kornelijuspacevicius@gmail.com>
2022-08-27 01:22:48 +0300

    QR code scanner

--- App.js
+++ App.js
@@ -1,66 +1,72 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
-import Constants from "expo-constants";
-import * as Permissions from "expo-permissions";
-
 import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default class BarcodeScannerExample extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    scanned: false,
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  // const completeChallenge = (points, user) => {
+  //   let userRef = doc(db, "users", user);
+
+  //   const data = getDoc(userRef);
+  //   setUserInfo(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+  //   alert(userInfo);
+  //   // let userData = { points: points, totalPoints: points };
+
+  //   // updateDoc(userRef, userData);
+  // };
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    let username, points, qrCodeData;
+
+    qrCodeData = data.split(" ");
+
+    username = qrCodeData[0];
+    points = qrCodeData[1];
+
+    if (points > 0) {
+      // alert(`Challenge completed. Added ${points} points to ${username}`);
+      // completeChallenge(points, user);
+    } else {
+      alert(`Reward claimed. Removed ${points * -1} points from ${username}`);
+    }
   };
 
-  async componentDidMount() {
-    this.getPermissionsAsync();
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
   }
 
-  getPermissionsAsync = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === "granted",
-    });
-  };
-
-  render() {
-    const { hasCameraPermission, scanned } = this.state;
-
-    if (hasCameraPermission === null) {
-      return <Text> Requesting for camera permission </Text>;
-    }
-    if (hasCameraPermission === false) {
-      return <Text> No access to camera </Text>;
-    }
-    return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          justifyContent: "flex-end",
-        }}
-      >
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? "" : this.handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
-        />
-        {scanned && (
-          <Button
-            title={"Tap to Scan Again"}
-            onPress={() =>
-              this.setState({
-                scanned: false,
-              })
-            }
-          />
-        )}{" "}
-      </View>
-    );
-  }
-
-  handleBarCodeScanned = ({ type, data }) => {
-    this.setState({
-      scanned: true,
-    });
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
